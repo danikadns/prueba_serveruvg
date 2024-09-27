69f258574bba78906d4a98c560cf43242d27628e
Elian Alvarado <107317821+TortrixBarbacoa@users.noreply.github.com>
2024-09-27 00:39:24 -0600

    Update generar_pedido.php

--- generar_pedido.php
+++ generar_pedido.php
@@ -17,26 +17,6 @@ $username = $_SESSION['username'];
 $sql = "INSERT INTO pedidos (cliente_username) VALUES ('$username')";
 $conn->query($sql);
 
-try {
-    $snsClient = new SnsClient([
-        'region' => 'us-east-1', // Cambia esto por tu región de AWS
-        'version' => 'latest',
-        'credentials' => [
-            'key'    => 'AKIAQE43J6UEEIEDDHZP',
-            'secret' => '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',
-        ],
-    ]);
-    
-    $result = $snsClient->publish([
-            'Message' => "Tu pedido se ha generado con éxito.",
-            'TopicArn' => 'arn:aws:sns:us-east-1:010526258440:uvgshopsns',
-        ]);
-
-    echo "Pedido actualizado y notificación enviada.";
-    } catch (AwsException $e) {
-        echo "Error al enviar notificación: " . $e->getMessage();
-    }
-
 $_SESSION['pedido_generado'] = true; 
 
 header('Location: index.php'); 
